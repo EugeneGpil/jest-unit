e1ec97e675379c7c946241543b6ddd693f4128e1
EugeneGpil <gpilligrim@gmail.com>
2021-10-12 22:24:54 +0700

    1

--- 02_sync/02_sync.js
+++ 02_sync/02_sync.js
@@ -1,6 +1,18 @@
 class Lodash {
-  compact(array) {}
-  groupBy(array, prop) {}
+  compact(array) {
+    return array.filter(value => value)
+  }
+
+  groupBy(array, prop) {
+    return array.reduce((resObj, value) => {
+      const propRes = typeof prop === 'function' ? prop(value) : value[prop]
+      if (!resObj[propRes]) {
+        resObj[propRes] = []
+      }
+      resObj[propRes].push(value)
+      return resObj
+    }, {})
+  }
 }
 
 module.exports = Lodash
--- 02_sync/sync.spec.js
+++ 02_sync/sync.spec.js
@@ -2,7 +2,25 @@ import Lodash from './02_sync'
 
 describe('Lodash: compact', () => {
 
-  const _ = new Lodash()
+  let _
+  let array
+
+  beforeAll(() => {
+    // _ = new Lodash()
+  })
+
+  beforeEach(() => {
+    _ = new Lodash()
+    array = [false, 42, 0, '', true, null, 'hello', undefined]
+  })
+
+  afterAll(() => {
+    //
+  })
+
+  afterEach(() => {
+    //
+  }) 
 
   test('should be definded', () => {
     expect(_.compact).toBeDefined()
@@ -10,7 +28,54 @@ describe('Lodash: compact', () => {
   })
 
   test('should remove falsy values from array', () => {
-    
+    const res = [42, true, 'hello']
+    expect(_.compact(array)).toStrictEqual(res)
   })
 
+  test('should return array without falsy values', () => {
+    expect(_.compact(array)).not.toContain(0)
+    expect(_.compact(array)).not.toContain('')
+    expect(_.compact(array)).not.toContain(null)
+    expect(_.compact(array)).not.toContain(false)
+    expect(_.compact(array)).not.toContain(undefined)
+  })
+
+})
+
+describe('Lodash: groupBy', () => {
+
+  let _
+
+  beforeEach(() => {
+    _ = new Lodash
+  })
+
+  test('should be definded', () => {
+    expect(_.groupBy).toBeDefined()
+    expect(_.groupBy).not.toBeUndefined()
+  })
+
+  test('should group array items by Math.floor', () => {
+    const array = [2.2, 2.4, 4.2, 3.1]
+    const res = {
+      2: [2.2, 2.4],
+      3: [3.1],
+      4: [4.2]
+    }
+    expect(_.groupBy(array, Math.floor)).toStrictEqual(res)
+  })
+
+  test('should group array items by length', () => {
+    const array = ['one', 'two', 'three']
+    const res = {
+      3: ['one', 'two'],
+      5: ['three']
+    }
+    expect(_.groupBy(array, 'length')).toStrictEqual(res)
+  })
+
+  test('shold return not array', () => {
+    const array = []
+    expect(_.groupBy(array, Math.trunc)).not.toBeInstanceOf(Array)
+  })
 })
